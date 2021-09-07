a8a3a69dc7ac17b77fb180afe6660793592717bc
yaroslav <76968721+versaproxx@users.noreply.github.com>
2021-09-07 22:46:06 +0300

    hotfix

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World, ReactJS!
+          Hello World, lolkekpenis
         </p>
         <a
           className="App-link"
